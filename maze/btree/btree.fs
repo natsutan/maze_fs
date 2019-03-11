9bdc483da7f635082180000051f9e2f326ff1848
natsutan <YRW03704@nifty.com>
2019-03-12 01:28:47 +0900

    fisrt commit

---
+++ maze/btree/btree.fs
@@ -0,0 +1,8 @@
+﻿// F# の詳細については、http://fsharp.org を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+open maze_lib
+
+[<EntryPoint>]
+let main argv = 
+    printfn "btree %A" argv
+    0 // 整数の終了コードを返します
---
+++ maze/maze_lib/maze_lib.fs
@@ -0,0 +1,4 @@
+﻿namespace maze_lib
+
+type Class1() = 
+    member this.X = "F#"

4a9ac5d71165ecad283a0c2dcae6560502543e5b
natsutan <YRW03704@nifty.com>
2019-03-13 19:09:16 +0900

    sidewinder追加

---
+++ maze/sidewinder/sidewinder.fs
@@ -0,0 +1,14 @@
+﻿open System 
+open maze_lib
+open maze_lib.Conv
+
+// F# の詳細については、http://fsharp.org を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+
+[<EntryPoint>]
+let main argv = 
+
+    let g = Grid(4, 4)
+    g.init()
+
+    0 // 整数の終了コードを返します
